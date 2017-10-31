ea2580e4525f1d336c1416456030650e981bb076
vrab <vrab>
2017-10-30 18:49:30 -0700

    Made xylophone keys.

---
+++ Assets/CubeOne.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeOne : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Renderer rend = GetComponent<Renderer>();
+		rend.material.shader = Shader.Find("Specular");
+		rend.material.SetColor("_SpecColor", Color.red);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
